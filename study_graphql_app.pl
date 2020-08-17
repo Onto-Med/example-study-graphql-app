36925600a02dff09352fd188f4f6c1f606785a8e
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2020-08-17 13:24:23 +0200

    Add mojolicious lite app

---
+++ study_graphql_app.pl
@@ -0,0 +1,43 @@
+use Mojolicious::Lite;
+use JSON::PP qw/decode_json/;
+use GraphQL::Schema;
+use Mojo::Collection qw/c/;
+
+app->config(hypnotoad => { listen => [ 'http://*:3000' ] });
+
+get '/' => { text => 'This is an example app for GraphQL studies. Navigate to /graphql for a graphiql interface.' };
+
+helper get_studies => sub {
+    my ($c, $args) = @_;
+    my $where   = $args->{where} || {};
+    my $options = $args->{options} || {};
+
+    state $studies = get_studies();
+
+    return $studies
+        ->head($options->{limit} // 10)
+        ->grep(sub { filter_studies($_, $where) })
+        ->to_array;
+};
+
+my $schema = Mojo::File->new(app->home->child('schema.graphql'))->slurp;
+plugin GraphQL => {
+    graphiql   => 1,
+    schema     => GraphQL::Schema->from_doc($schema),
+    root_value => { studies => sub { app->get_studies(shift) } },
+};
+
+app->start;
+
+
+sub get_studies {
+    c(@{decode_json(Mojo::File->new(app->home->child('studies.json'))->slurp)});
+}
+
+sub filter_studies {
+    my $study  = shift;
+    my $where = shift;
+
+    return $study->{id} == $where->{id} if (length $where->{id});
+    return 1;
+}
